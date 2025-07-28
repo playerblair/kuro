ac131ebb62af6359af78c5139eee44a80ceb044a
Blair Akinmade <156689303+playerblair@users.noreply.github.com>
2025-07-28 23:00:11 +0100

    refactor MangaControllerTest

--- src/test/java/dev/playerblair/kuro/manga/MangaControllerTest.java
+++ src/test/java/dev/playerblair/kuro/manga/MangaControllerTest.java
@@ -13,20 +13,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Set;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureJsonTesters
 @WebMvcTest(MangaController.class)
@@ -38,9 +36,6 @@ public class MangaControllerTest {
     @MockitoBean
     private MangaService mangaService;
 
-    @MockitoBean
-    private JwtDecoder jwtDecoder;
-
     @Autowired
     private JacksonTester<MangaSearchResponse> jsonMangaSearchResponse;
 
@@ -49,7 +44,6 @@ public class MangaControllerTest {
 
     private MangaResponse mangaResponse;
     private MangaSearchResponse mangaSearchResponse;
-    private Manga manga;
 
     @BeforeEach
     public void setUp() {
@@ -72,7 +66,7 @@ public class MangaControllerTest {
                 new Pagination(1, false, 1),
                 Set.of(mangaDto)
         );
-        manga = Manga.builder()
+        Manga manga = Manga.builder()
                 .malId(mangaDto.malId())
                 .title(mangaDto.title())
                 .titleEnglish(mangaDto.titleEnglish())
@@ -94,20 +88,16 @@ public class MangaControllerTest {
         // given
         String query = "Test Manga";
         int page = 1;
-        given(mangaService.searchManga(query, page)).willReturn(mangaSearchResponse);
+        when(mangaService.searchManga(query, page)).thenReturn(mangaSearchResponse);
 
-        // when
-        MockHttpServletResponse response = mockMvc.perform(
-                get("/api/manga/search")
-                        .param("query", query)
-                        .param("page", String.valueOf(page))
-                        .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
-
-        // then
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.getContentAsString())
-                .isEqualTo(jsonMangaSearchResponse.write(mangaSearchResponse).getJson());
+        // when & then
+        mockMvc.perform(get("/api/manga/search")
+                    .param("query", query)
+                    .param("page", String.valueOf(page))
+                    .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(jsonMangaSearchResponse.write(mangaSearchResponse).getJson()));
+        verify(mangaService).searchManga(query, page);
     }
 
     @Test
@@ -117,17 +107,14 @@ public class MangaControllerTest {
         String query = "";
         int page = 0;
 
-        // when
-        MockHttpServletResponse response = mockMvc.perform(
-                        get("/api/manga/search")
-                                .param("query", query)
-                                .param("page", String.valueOf(page))
-                                .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
-
-        // then
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-        assertThat(response.getContentAsString()).contains("query", "page");
+        // when & then
+        mockMvc.perform(get("/api/manga/search")
+                    .param("query", query)
+                    .param("page", String.valueOf(page))
+                    .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(allOf(containsString("query"), containsString("page"))));
     }
 
     @Test
@@ -136,19 +123,18 @@ public class MangaControllerTest {
         // given
         String query = "Test Manga";
         int page = 1;
-        given(mangaService.searchManga(query, page)).willThrow(new JikanApiException("Unexpected error occurred while accessing Jikan API"));
+        when(mangaService.searchManga(query, page)).thenThrow(new JikanApiException("Unexpected error occurred while accessing Jikan API"));
 
-        // when
-        MockHttpServletResponse response = mockMvc.perform(
-                        get("/api/manga/search")
-                                .param("query", query)
-                                .param("page", String.valueOf(page))
-                                .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
-
-        // then
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        assertThat(response.getContentAsString()).contains("500", "Unexpected error occurred while accessing Jikan API");
+        // when & then
+        mockMvc.perform(get("/api/manga/search")
+                    .param("query", query)
+                    .param("page", String.valueOf(page))
+                    .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is5xxServerError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(allOf(containsString("500"),
+                        containsString("Unexpected error occurred while accessing Jikan API"))));
+        verify(mangaService).searchManga(query, page);
     }
 
     @Test
@@ -156,13 +142,12 @@ public class MangaControllerTest {
         // given
         String query = "Test Manga";
         int page = 1;
-        given(mangaService.searchManga(query, page)).willReturn(mangaSearchResponse);
 
         // when & then
         mockMvc.perform(get("/api/manga/search")
-                            .param("query", query)
-                            .param("page", String.valueOf(page))
-                            .accept(MediaType.APPLICATION_JSON))
+                    .param("query", query)
+                    .param("page", String.valueOf(page))
+                    .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isUnauthorized());
     }
 
@@ -171,17 +156,15 @@ public class MangaControllerTest {
     public void getManga_shouldReturn200AndResponse() throws Exception {
         // given
         Long malId = 1L;
-        given(mangaService.getManga(malId)).willReturn(mangaResponse);
+        when(mangaService.getManga(malId)).thenReturn(mangaResponse);
 
-        // when
-        MockHttpServletResponse response = mockMvc.perform(get("/api/manga/{malId}", malId)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
-
-        // then
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.getContentAsString())
-                .isEqualTo(jsonMangaResponse.write(mangaResponse).getJson());
+        // when & then
+        mockMvc.perform(get("/api/manga/{malId}", malId)
+                    .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(jsonMangaResponse.write(mangaResponse).getJson()));
+        verify(mangaService).getManga(malId);
     }
 
     @Test
@@ -190,14 +173,12 @@ public class MangaControllerTest {
         // given
         Long malId = 0L;
 
-        // when
-        MockHttpServletResponse response = mockMvc.perform(get("/api/manga/{malId}", malId)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
-
-        // then
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-        assertThat(response.getContentAsString()).contains("malId");
+        // when & then
+        mockMvc.perform(get("/api/manga/{malId}", malId)
+                    .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(containsString("malId")));
     }
 
     @Test
@@ -205,16 +186,15 @@ public class MangaControllerTest {
     public void getManga_givenMangaNotFound_shouldReturn404AndErrorResponse() throws Exception {
         // given
         Long malId = 3L;
-        given(mangaService.getManga(malId)).willThrow(new JikanApiMangaNotFoundException(malId));
+        when(mangaService.getManga(malId)).thenThrow(new JikanApiMangaNotFoundException(malId));
 
-        // when
-        MockHttpServletResponse response = mockMvc.perform(get("/api/manga/{malId}", malId)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
-
-        // then
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
-        assertThat(response.getContentAsString()).contains("Manga not found with malID: " + malId);
+        // when & then
+        mockMvc.perform(get("/api/manga/{malId}", malId)
+                    .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(containsString("Manga not found with malID: " + malId)));
+        verify(mangaService).getManga(malId);
     }
 
     @Test
@@ -222,16 +202,16 @@ public class MangaControllerTest {
     public void getManga_givenClientFails_shouldReturn5xxAndErrorResponse() throws Exception {
         // given
         Long malId = 1L;
-        given(mangaService.getManga(malId)).willThrow(new JikanApiException("Unexpected error occurred while accessing Jikan API"));
+        when(mangaService.getManga(malId)).thenThrow(new JikanApiException("Unexpected error occurred while accessing Jikan API"));
 
-        // when
-        MockHttpServletResponse response = mockMvc.perform(get("/api/manga/{malId}", malId)
+        // when & then
+        mockMvc.perform(get("/api/manga/{malId}", malId)
                         .accept(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
-
-        // then
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        assertThat(response.getContentAsString()).contains("Unexpected error occurred while accessing Jikan API");
+                .andExpect(status().is5xxServerError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(allOf(containsString("500"),
+                        containsString("Unexpected error occurred while accessing Jikan API"))));
+        verify(mangaService).getManga(malId);
     }
 
     @Test
