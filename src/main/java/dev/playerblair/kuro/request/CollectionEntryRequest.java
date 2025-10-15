3cd5e4d5832b7beb442ab8d4939c3f7133e0be7a
Blair Michael Akinmade <156689303+playerblair@users.noreply.github.com>
2025-10-15 20:40:10 +0100

    add new validations and error messages

--- src/main/java/dev/playerblair/kuro/request/CollectionEntryRequest.java
+++ src/main/java/dev/playerblair/kuro/request/CollectionEntryRequest.java
@@ -2,14 +2,24 @@ package dev.playerblair.kuro.request;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
 public record CollectionEntryRequest(
-        @Min(value = 0, groups = ValidationGroups.Create.class) Long malId,
-        @NotBlank(groups = ValidationGroups.Create.class) String edition,
-        @Min(value = 0, groups = ValidationGroups.Create.class) Integer volumeNumber,
+        @NotNull(message = "{not_null.malId}")
+        @Min(value = 1, message = "{min.malId}", groups = ValidationGroups.Create.class)
+        Long malId,
+
+        @NotBlank(message = "Edition cannot be blank", groups = ValidationGroups.Create.class)
+        String edition,
+
+        @NotNull(message = "Volume no. cannot be null")
+        @Min(value = 0, message = "Volume no. cannot be less than 0", groups = ValidationGroups.Create.class)
+        Integer volumeNumber,
+
         String notes,
+
         LocalDate purchaseDate
 ) {
 }
