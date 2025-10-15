774b130bc3883ebee6bbf5f6447ace582b207db5
Blair Michael Akinmade <156689303+playerblair@users.noreply.github.com>
2025-10-15 20:42:54 +0100

    add @RequestBody annotation to create update and delete methods, amend update and delete methods to return SimpleResponse

--- src/main/java/dev/playerblair/kuro/controller/LibraryController.java
+++ src/main/java/dev/playerblair/kuro/controller/LibraryController.java
@@ -1,11 +1,13 @@
 package dev.playerblair.kuro.controller;
 
 import dev.playerblair.kuro.dto.LibraryEntryDto;
+import dev.playerblair.kuro.dto.SimpleResponse;
 import dev.playerblair.kuro.model.LibraryEntry;
 import dev.playerblair.kuro.request.LibraryEntryRequest;
 import dev.playerblair.kuro.request.ValidationGroups;
 import dev.playerblair.kuro.service.LibraryService;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.groups.Default;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/library")
+@Validated
 public class LibraryController {
 
     private final LibraryService libraryService;
@@ -33,8 +36,9 @@ public class LibraryController {
     }
 
     @GetMapping("/{malId}")
-    public ResponseEntity<LibraryEntryDto> getLibraryEntry
-            (@PathVariable @Min(0) Long malId, Authentication authentication) {
+    public ResponseEntity<LibraryEntryDto> getLibraryEntry(
+            @PathVariable @NotNull(message = "{not_null.malId}") @Min(value = 1, message = "{min.malId}") Long malId,
+            Authentication authentication) {
         return ResponseEntity.ok(libraryService.getLibraryEntry(malId, authentication).toDto());
     }
 
@@ -48,17 +52,21 @@ public class LibraryController {
     }
 
     @PatchMapping("/{malId}")
-    public ResponseEntity<Void> updateLibraryEntry(
-            @PathVariable Long malId,
+    public ResponseEntity<SimpleResponse> updateLibraryEntry(
+            @PathVariable @NotNull(message = "{not_null.malId}") @Min(value = 1, message = "{min.malId}") Long malId,
             @Validated(Default.class) @RequestBody LibraryEntryRequest request,
             Authentication authentication) {
         libraryService.updateLibraryEntry(malId, request, authentication);
-        return ResponseEntity.noContent().build();
+        SimpleResponse response = new SimpleResponse("LibraryEntry updated successfully");
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{malId}")
-    public ResponseEntity<Void> deleteLibraryEntry(@PathVariable Long malId, Authentication authentication) {
+    public ResponseEntity<SimpleResponse> deleteLibraryEntry(
+            @PathVariable @NotNull(message = "{not_null.malId}") @Min(value = 1, message = "{min.malId}") Long malId,
+            Authentication authentication) {
         libraryService.deleteLibraryEntry(malId, authentication);
-        return ResponseEntity.noContent().build();
+        SimpleResponse response = new SimpleResponse("LibraryEntry deleted successfully");
+        return ResponseEntity.ok(response);
     }
 }
