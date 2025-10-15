3808c90b4dfbde91c672559a4ccbf86298c09e60
Blair Michael Akinmade <156689303+playerblair@users.noreply.github.com>
2025-10-15 20:41:35 +0100

    add validation annotations and error messages

--- src/main/java/dev/playerblair/kuro/request/LibraryEntryRequest.java
+++ src/main/java/dev/playerblair/kuro/request/LibraryEntryRequest.java
@@ -1,16 +1,26 @@
 package dev.playerblair.kuro.request;
 
 import dev.playerblair.kuro.model.Progress;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 public record LibraryEntryRequest(
-        @Min(value = 0, groups = ValidationGroups.Create.class) Long malId,
+        @NotNull(message = "{not_null.malId}")
+        @Min(value = 1, message = "{min.malId}", groups = ValidationGroups.Create.class)
+        Long malId,
+
+        @NotNull(message = "Progress cannot be null")
         Progress progress,
-        @Min(0) Integer chaptersRead,
-        @Min(0)Integer volumesRead,
-        @Min(0) @Max(10) Integer rating,
+
+        @Min(value = 0, message = "Chapters read cannot be less than 0")
+        Integer chaptersRead,
+
+        @Min(value = 0, message = "Volumes read cannot be less than 0")
+        Integer volumesRead,
+
+        @Min(value = 1, message = "Rating cannot be lower than 1")
+        @Max(value = 10, message = "Rating cannot be greater than 10")
+        Integer rating,
+
         String notes
 ) {
 }
