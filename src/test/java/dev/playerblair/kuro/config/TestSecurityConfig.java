44f6b9778fe44de5d21b336776cd1dc8deab21e9
Blair Michael Akinmade <156689303+playerblair@users.noreply.github.com>
2025-10-15 20:45:32 +0100

    add TestSecurityConfig

---
+++ src/test/java/dev/playerblair/kuro/config/TestSecurityConfig.java
@@ -0,0 +1,20 @@
+package dev.playerblair.kuro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class TestSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain testFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/signup").permitAll()
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .anyRequest().authenticated())
+                .build();
+    }
+}
